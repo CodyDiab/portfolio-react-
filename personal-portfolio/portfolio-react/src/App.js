478d892d4655d4658ed3bbdc9b29f21ae56360bc
Diab <emailcodydiab@gmail.com>
2020-07-31 07:19:10 -0500

    init about componenet layout

--- personal-portfolio/portfolio-react/src/App.js
+++ personal-portfolio/portfolio-react/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import About from './components/About'
+
+import './styles/index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header className="header" >
+        <h1>Here is the title, howdy yall</h1>
       </header>
+      <div>
+         <About></About>
+      </div>
+     
     </div>
   );
 }
